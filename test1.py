87a6c231e76723cb543bb690ad6dfa3f8c2cc1a8
RohanSinghWhjCoder <70446178+rohansinghgamer@users.noreply.github.com>
2022-02-05 14:57:43 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,9 @@
+#var1 = "kutta kamina"
+"""var2= 345.76
+var3= 54
+print(var3*"heheboi\n")"""
+print("apna naam daal")
+n1: str= input()
+print("apni/apna wali/wala ka naam daal")
+n2: str= input()
+print("jalwa dekh ❤", str(n1) +str(n2))
